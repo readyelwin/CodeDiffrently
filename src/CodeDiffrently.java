11640ba8da7c70456e7c6a76c0ee2e165a65a4d5
Marie Ready <readyelwin@gmail.com>
2020-10-05 22:06:23 -0400

    changes made to this

--- src/CodeDiffrently.java
+++ src/CodeDiffrently.java
@@ -2,29 +2,22 @@
 
 
 
-public class DogWalkerTesting {
+public class DogWalking {
 
 	public static void main(String[] args) {
-		@Test
-		public void firstTest() {
-			 DogWalker dw = new DogWalker();
-		        int[] dogs = {1, 1, 3, 1, 2, 1, 3, 3, 3, 3};
-		        int expected = dw.numberOfWalksCalculator(dogs);
-		        int actual = 6;
-		        Assert.assertEquals(expected, actual);
-		    }
-		    @Test
-		    public void secondTest(){
-		        DogWalker dw = new DogWalker();
-		        int[] dogs = {10, 20, 10, 10, 30, 50, 10, 20, 30};
-		        int expected = dw.numberOfWalksCalculator(dogs);
-		        int actual = 6;
-		        int actual = 5;
-		        Assert.assertEquals(expected, actual);
-		    }
-		}
 		
-		
+		public void DogWalker() {
+			 
+		        int[] dogs = {1, 1, 1, 2, 1, 3, 3};
+		        int walks = 4;
+		        int numberOfDogs= dogs;
+		        int dogsToWalk = 6;
+		        int hours = walks;
+		       
+		       System.out.println("Number of Dogwalks that he has to make is " walks);
+		    }
+		       
+		}	
 	
 	}
 
